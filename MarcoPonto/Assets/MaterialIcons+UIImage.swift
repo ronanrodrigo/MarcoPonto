e823e3b82f92032a42a561a5f86ebfd7ee29bfda
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-04-19 19:09:34 -0300

    New assets

---
+++ MarcoPonto/Assets/MaterialIcons+UIImage.swift
@@ -0,0 +1,20 @@
+//
+//  MaterialIcons.swift
+//  MarcoPonto
+//
+//  Created by Ronan Rodrigo Nunes on 19/04/16.
+//  Copyright © 2016 Ronan Rodrigo Nunes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    static var accessTime: UIImage {
+        get { return UIImage(named: "ic_access_time")! }
+    }
+    
+    static var dateRange: UIImage {
+        get { return UIImage(named: "ic_date_range")! }
+    }
+}
