14f9cd80ed812c16a2ca1e6911691b179bffe009
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-04-22 11:47:24 -0300

    Tableview at reports

--- MarcoPonto/ViewsControllers/Reports/TotalsViewController.swift
+++ MarcoPonto/ViewsControllers/Reports/TotalsViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class TotalsViewController: UIViewController, CurrentWeekHoursPresenter {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
