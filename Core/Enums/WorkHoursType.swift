ffcae93707d9e2686c6b9b42b5459ce1cbc22780
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-05-02 13:56:39 -0300

    Added WorkHoursType

---
+++ Core/Enums/WorkHoursType.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+enum WorkHoursType {
+    case PastWeek, CurrentWeek, PastMonth, CurrentMonth, Total
+
+    var title: String {
+        get {
+            switch self {
+                case .PastWeek: return "Semana passada"
+                case .CurrentWeek: return "Semana atual"
+                case .PastMonth: return "Mês passado"
+                case .CurrentMonth: return "Mês atual"
+                case .Total: return "Tudo"
+            }
+        }
+    }
+}
