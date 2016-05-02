6c1856e45518188df6c69b99dc9935e67bacb9bc
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-05-02 14:28:04 -0300

    Added balance property to WorkHour

--- Core/Structs/WorkHour.swift
+++ Core/Structs/WorkHour.swift
@@ -3,9 +3,11 @@ import Foundation
 protocol WorkHour {
     var type: WorkHourType { get set }
     var total: NSTimeInterval { get set }
+    var balance: NSTimeInterval { get set }
 }
 
 struct WorkHourStruct: WorkHour {
     var type: WorkHourType
     var total: NSTimeInterval
+    var balance: NSTimeInterval
 }
