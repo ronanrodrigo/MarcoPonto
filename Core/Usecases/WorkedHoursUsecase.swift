5f91b9846765b7915ccf3ab0d4b6ae4eaeaf6b62
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-05-02 14:20:44 -0300

    Changed calculate function to return WorkHourStruct

--- Core/Usecases/WorkedHoursUsecase.swift
+++ Core/Usecases/WorkedHoursUsecase.swift
@@ -14,57 +14,48 @@ class WorkedHoursUsecase {
 
     func total() {
         let pastWeek = dateRangeGateway.pastWeek()
-        let pastWeekTotalHours = calculateHours(
+        let pastWeekWorkHours = calculateHours(
             between: pastWeek.firstDate,
-            and: pastWeek.lastDate)
-        let pastWeekWorkWorkHours = WorkHourStruct(
-            type: .PastWeek,
-            total: pastWeekTotalHours)
-        presenter.showTotal(pastWeekWorkWorkHours)
+            and: pastWeek.lastDate,
+            type: .PastWeek)
+        presenter.showTotal(pastWeekWorkHours)
 
         let currentWeek = dateRangeGateway.currentWeek()
-        let currentWeekTotalHours = calculateHours(
+        let currentWeekWorkHours = calculateHours(
             between: currentWeek.firstDate,
-            and: currentWeek.lastDate)
-        let currentWeekWorkHours = WorkHourStruct(
-            type: .CurrentWeek,
-            total: currentWeekTotalHours)
+            and: currentWeek.lastDate,
+            type: .CurrentWeek)
         presenter.showTotal(currentWeekWorkHours)
 
         let pastMonth = dateRangeGateway.pastMonth()
-        let pastMonthTotalHours = calculateHours(
+        let pastMonthWorkHours = calculateHours(
             between: pastMonth.firstDate,
-            and: pastMonth.lastDate)
-        let pastMonthWorkHours = WorkHourStruct(
-            type: .PastMonth,
-            total: pastMonthTotalHours)
+            and: pastMonth.lastDate,
+            type: .PastMonth)
         presenter.showTotal(pastMonthWorkHours)
 
         let currentMonth = dateRangeGateway.currentMonth()
-        let currentMonthTotalHours = calculateHours(
+        let currentMonthWorkHours = calculateHours(
             between: currentMonth.firstDate,
-            and: currentMonth.lastDate)
-        let currentMonthWorkHours = WorkHourStruct(
-            type: .CurrentMonth,
-            total: currentMonthTotalHours)
+            and: currentMonth.lastDate,
+            type: .CurrentMonth)
         presenter.showTotal(currentMonthWorkHours)
 
         let fromBeginning = dateRangeGateway.fromBeginning()
-        let fromBeginningTotalHours = calculateHours(
+        let fromBeginningWorkHours = calculateHours(
             between: fromBeginning.firstDate,
-            and: fromBeginning.lastDate)
-        let fromBeginningWorkHours = WorkHourStruct(
-            type: .All,
-            total: fromBeginningTotalHours)
+            and: fromBeginning.lastDate,
+            type: .All)
         presenter.showTotal(fromBeginningWorkHours)
     }
 
-    private func calculateHours(between firstDate: NSDate, and lastDate: NSDate) -> NSTimeInterval {
+    private func calculateHours(between firstDate: NSDate, and lastDate: NSDate, type: WorkHourType) -> WorkHourStruct {
         let inputPunchs = gateway.list(by: .Input, between: firstDate, and: lastDate)
         let outputPunchs = gateway.list(by: .Output, between: firstDate, and: lastDate)
         let totalInterval = CalculateWorkedHoursEntity(
             inputPunchs: inputPunchs,
             outputPunchs: outputPunchs).calculate()
-        return totalInterval
+
+        return WorkHourStruct(type: type, total: totalInterval)
     }
 }
