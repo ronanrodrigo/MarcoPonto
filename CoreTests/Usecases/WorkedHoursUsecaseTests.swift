2f63037341a44325b7e438b06af188fbc33bdd8c
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-05-03 19:45:58 -0300

    Removed daterangegateway and created NSDate extension

--- CoreTests/Usecases/WorkedHoursUsecaseTests.swift
+++ CoreTests/Usecases/WorkedHoursUsecaseTests.swift
@@ -5,14 +5,11 @@ class WorkedHoursUsecaseTests: XCTestCase {
     var usecase: WorkedHoursUsecase!
     var presenter: WorkedHoursPresenterFake!
     var gateway: PunchGatewaySpy!
-    var dateRangeGateway: DateRangeGatewayFake!
 
     override func setUp() {
         gateway = PunchGatewaySpy()
-        dateRangeGateway = DateRangeGatewayFake()
         presenter = WorkedHoursPresenterFake()
-        usecase = WorkedHoursUsecase(
-            gateway: gateway, dateRangeGateway: dateRangeGateway, presenter: presenter)
+        usecase = WorkedHoursUsecase(gateway: gateway, presenter: presenter)
     }
 
     func testShouldHaveCompleteWokedHoursReport() {
@@ -20,15 +17,5 @@ class WorkedHoursUsecaseTests: XCTestCase {
 
         XCTAssertEqual(5, presenter.workHours.count)
         XCTAssertEqual(10, gateway.listSpieds.count)
-    }
-
-    func testShouldHaveGetAllDateRanges() {
-        usecase.total()
-
-        XCTAssertTrue(dateRangeGateway.pastWeekSpied)
-        XCTAssertTrue(dateRangeGateway.currentWeekSpied)
-        XCTAssertTrue(dateRangeGateway.pastMonthSpied)
-        XCTAssertTrue(dateRangeGateway.currentMonthSpied)
-        XCTAssertTrue(dateRangeGateway.fromBeginningSpied)
     }
 }
