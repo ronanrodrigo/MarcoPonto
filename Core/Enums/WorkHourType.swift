09fe338ecb5233b4e71d8fdb412694a7c43b66bc
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-05-03 20:00:25 -0300

    Added dateRange and types properties

--- Core/Enums/WorkHourType.swift
+++ Core/Enums/WorkHourType.swift
@@ -3,15 +3,27 @@ import Foundation
 enum WorkHourType {
     case PastWeek, CurrentWeek, PastMonth, CurrentMonth, All
 
+    static var types: [WorkHourType] {
+        return [.PastWeek, .CurrentWeek, .PastMonth, .CurrentMonth, .All]
+    }
+
     var title: String {
-        get {
-            switch self {
-                case .PastWeek: return "Semana passada"
-                case .CurrentWeek: return "Semana atual"
-                case .PastMonth: return "Mês passado"
-                case .CurrentMonth: return "Mês atual"
-                case .All: return "Tudo"
-            }
+        switch self {
+            case .PastWeek: return "Semana passada"
+            case .CurrentWeek: return "Semana atual"
+            case .PastMonth: return "Mês passado"
+            case .CurrentMonth: return "Mês atual"
+            case .All: return "Tudo"
+        }
+    }
+
+    var dateRange: (firstDate: NSDate, lastDate: NSDate) {
+        switch self {
+            case .PastWeek: return NSDate.pastWeek
+            case .CurrentWeek: return NSDate.currentWeek
+            case .PastMonth: return NSDate.pastMonth
+            case .CurrentMonth: return NSDate.currentMonth
+            case .All: return NSDate.fromBeginning
         }
     }
 }
