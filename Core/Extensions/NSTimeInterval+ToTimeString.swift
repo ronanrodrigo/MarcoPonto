457dbd2b487111fa8e3004ee66dbc1785529fd36
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-05-04 09:32:46 -0300

    Updated extension to get absolute values of minutes/seconds

--- Core/Extensions/NSTimeInterval+ToTimeString.swift
+++ Core/Extensions/NSTimeInterval+ToTimeString.swift
@@ -6,6 +6,6 @@ extension NSTimeInterval {
         let seconds = interval % 60
         let minutes = (interval / 60) % 60
         let hours = (interval / 3600)
-        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        return String(format: "%02d:%02d:%02d", hours, abs(minutes), abs(seconds))
     }
 }
