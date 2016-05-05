ce5d79a8a104f33bbdecfb1c9b535bcadb16a9c5
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-05-05 13:32:09 -0300

    Updated hour formation

--- MarcoPontoCore/MarcoPontoCore/Extensions/NSTimeInterval+ToTimeString.swift
+++ MarcoPontoCore/MarcoPontoCore/Extensions/NSTimeInterval+ToTimeString.swift
@@ -6,6 +6,6 @@ public extension NSTimeInterval {
         let seconds = interval % 60
         let minutes = (interval / 60) % 60
         let hours = (interval / 3600)
-        return String(format: "%02d:%02d:%02d", hours, abs(minutes), abs(seconds))
+        return String(format: "%02dh %02dm", hours, abs(minutes), abs(seconds))
     }
 }
