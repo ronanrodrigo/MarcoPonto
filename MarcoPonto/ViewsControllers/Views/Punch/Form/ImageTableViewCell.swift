9acc0e09872d237aeb60834a0a970e21e2cac404
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-05-07 01:09:02 -0300

    Created punch card image view cell

--- MarcoPonto/ViewsControllers/Views/Punch/Form/ImageTableViewCell.swift
+++ MarcoPonto/ViewsControllers/Views/Punch/Form/ImageTableViewCell.swift
@@ -1,24 +1,7 @@
-//
-//  ImageTableViewCell.swift
-//  MarcoPonto
-//
-//  Created by Ronan Rodrigo Nunes on 07/05/16.
-//  Copyright © 2016 Ronan Rodrigo Nunes. All rights reserved.
-//
-
 import UIKit
 
 class ImageTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+    @IBOutlet weak var punchCardPhoto: UIImageView!
 
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
